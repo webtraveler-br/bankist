e88eeadac12fb78c67e8573f27e8cbdc9dbce59d
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2022-05-03 23:05:55 -0300

    :tada: project start and new movements history feature

---
+++ alternativeCoolCode.js
@@ -0,0 +1,26 @@
+// alternative to displayMovements using node creation and fragments
+function displayMovements(movements) {
+  containerMovements.textContent = '';
+  const rootClass = 'movements__'
+  const movementRows = document.createDocumentFragment();
+  movements.forEach((data) => {
+    const movementType = data > 0 ? 'deposit' : 'withdrawal';
+    const row = document.createElement('div');
+    const value = document.createElement('div');
+    const date = document.createElement('div'); 
+    const type = document.createElement('div');
+    row.classList.add(`${rootClass}row`);
+    value.classList.add(`${rootClass}value`);
+    date.classList.add(`${rootClass}date`);
+    type.classList.add(`${rootClass}type`);
+    type.classList.add(`${rootClass}type--${movementType}`);
+    value.innerText = moneyFormatter.format(data);
+    type.textContent = `1 ${movementType}`;
+    date.textContent = '1 day ago';
+    row.appendChild(type);
+    row.appendChild(date);
+    row.appendChild(value);
+    movementRows.appendChild(row);
+  });
+  containerMovements.appendChild(movementRows);
+}
