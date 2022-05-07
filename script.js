37cb58e9b9a5eb8385831427e301eef39ab011e2
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2022-05-07 07:36:30 -0300

    :coffin: learning code

--- script.js
+++ script.js
@@ -109,6 +109,3 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
-const deposits = movements.filter(mov => mov > 0);
-const withdrawals = movements.filter(mov => mov < 0);
